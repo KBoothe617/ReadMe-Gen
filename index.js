c6a1e9a010bc82be3b383eb374196abea1a75bad
Kaleb Boothe <kboothe617@gmail.com>
2024-07-29 14:26:42 -0600

    working on array user input

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
