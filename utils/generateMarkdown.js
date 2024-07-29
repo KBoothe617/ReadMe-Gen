dc76c5f4da6ecb8dac65f19a3ed6008a5e963573
Kaleb Boothe <kboothe617@gmail.com>
2024-07-29 16:33:11 -0600

    license switch with license links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,8 +9,23 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+  switch (license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+    case 'Apache':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    case 'GPL':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+    case 'BSD':
+      return 'https://opensource.org/licenses/BSD-3-Clause';
+    default:
+      return '';
+  }
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
